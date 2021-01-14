70d42ba2c71ab7f997a169a218c9745243dc8025
James Abbott <jamesbabbott@gmail.com>
2021-01-14 21:45:51 +0000

    Add simplified syntax

--- vars/invokePsake.groovy
+++ vars/invokePsake.groovy
@@ -1,9 +1,10 @@
 
 def call(String target, String label = '', String psakeFile = './psakefile.ps1') {
-  String buildLabel = target
+  String buildLabel = "Invoke-psake: ${target}"
 
   if (label != '') {
-    buildLabel = target
+    buildLabel = label
   }
+
   pwsh script: "Invoke-psake ${psakeFile} ${target}; exit !(\$psake.build_success)", label: "${buildLabel}"
 }
